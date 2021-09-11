2d56fa92d099d9a8b658bf57befd563f185daa5d
F-R-Mitchell <87910875+F-R-Mitchell@users.noreply.github.com>
2021-09-11 10:39:34 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -13,10 +13,7 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("C:\\Users\\FrantheMan\\Desktop\\Classwork\\resources\\Election_Analysis\\analysis\\election_analysis.txt")
-outfile = open(file_to_save, "w")
-outfile.write("Hello World")
 
-outfile.close()
 # Initializing the vote counter
 total_votes = 0
 
@@ -54,21 +51,29 @@ with open(file_to_load) as election_data:
 
         candidate_votes[candidate_name] += 1
 
-    for candidate_name in candidate_options:
+with open(file_to_save,"w") as txt_file:
+    election_results =(
+        f"Election Results\n"
+        f"-----------------------\n"
+        f"Total Votes:{total_votes}\n"
+        f"-----------------------\n")
+    print(election_results, end="")
+    
+    txt_file.write(election_results)
+  
+    for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes)/float(total_votes)*100
         formatted_vote_percentage = format(vote_percentage,".1f")
+            
+# Print out the winning candidate, vote count, and percentage
+
+        txt_file.write(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
         
-    
         if (votes > winning_count) and (vote_percentage> winning_percentage):
             winning_count = votes
             winning_percentage = vote_percentage
-            winning_candidate = candidate_name
-
-
-# Print out the winning candidate, vote count, and percentage
-
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            winning_candidate = candidate_name    
     
     winning_candidate_summary = (
         f"-------------------------\n"
@@ -77,3 +82,5 @@ with open(file_to_load) as election_data:
         f"Winning Percentage:{winning_percentage:.1f}%\n"
         f"-------------------------\n")
     print(winning_candidate_summary)
+
+    txt_file.write(winning_candidate_summary)
